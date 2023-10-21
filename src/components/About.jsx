08768ccaffc905841507bb756675e99a09e080c8
AyaobaEstelle <abolarinesther98@gmail.com>
2023-10-21 16:47:21 +0100

    updated css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,14 +5,17 @@ import { NavLink } from "react-router-dom";
   return (
     <div className="about clr-neutral-100">
       <h1 className="fs-700 fw-bold">counter</h1>
-      <h3 className="fs-400">AltSchool Second Semester <strong>Examination Project</strong></h3>
+      <h3 className="fs-400"><a href="https://altschoolafrica.com/">AltSchool</a> Second Semester <strong>Examination Project</strong></h3>
       <p>A numeric Counter App with interactive buttons for <strong className="clr-primary-600">Increment and Decrement</strong></p>
       <ul>
-        <li>INCREMENT: click the 'Increment' button <span>to increase the count by 1</span></li>
-        <li>DECREMENT: click the 'Decrement' button <span>to decrease the count by 1</span></li>
-        <li>RESET: click the 'Reset' button <span>to reset count to 0</span></li>
+        <li>Enter new value: input any number <span>to set the count value.</span></li>
+        <li>Set value: click the 'set value' button <span>to place the value on the counter.</span></li>
+        <li>Increment: click the 'Increment' button <span>to increase the count by 1.</span></li>
+        <li>Decrement: click the 'Decrement' button <span>to decrease the count by 1.</span></li>
+        <li>Reset: click the 'Reset' button <span>to reset count to 0.</span></li>
       </ul>
-      <p><a className="clr-neutral-100" href="https://github.com/AyaobaEstelle/AltSchool-Second-Semester-Examination-Project">GitHub Repository URL</a></p>
+      <small>Note: The counter will not 'Set Value' <span>for Alphabets only NUMBERS</span></small>
+      <p><a href="https://github.com/AyaobaEstelle/AltSchool-Second-Semester-Examination-Project">GitHub Repository URL</a></p>
     </div>
   );
 }
--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react'
  function NotFound() {
   return (
     <div className='not-found'> 
-      <h1 className='clr-primary-700 fw-bold fs-900'>404</h1>
+      <h1 className='clr-primary-700 fw-bold'>404</h1>
       <h2 className='fs-500'>opps! page not found</h2>
       <p className='fs-600'>The page you are looking for <strong className='clr-primary-700'> does not exist</strong>.</p>
       <a href="./">
